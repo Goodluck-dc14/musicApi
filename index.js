10da8b9195ef976c3af202ef0ae8afa526748cdf
Goodluck-dc14 <84187549+Goodluck-dc14@users.noreply.github.com>
2021-10-15 16:12:50 +0100

    music api

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 2001;
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 app.use(express.json());
 
-// mongoose.connect(`${process.env.MONGODB_URI}`, {
-//   useNewUrlParser: true,
-// });
+mongoose.connect(`${process.env.MONGODB_URI}`, {
+  useNewUrlParser: true,
+});
 
-// mongoose.connection
-//   .on("open", (req, res) => {
-//     console.log("Database is connected successfully");
-//   })
-//   .once("error", (req, res) => {
-//     console.log("failed to connect to database");
-//   });
+mongoose.connection
+  .on("open", (req, res) => {
+    console.log("Database is connected successfully");
+  })
+  .once("error", (req, res) => {
+    console.log("failed to connect to database");
+  });
 
 app.get("/", async (req, res) => {
   res.send("music api build by codelab student");
 });
 
-// app.use(cors());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`server is listening on port: ${port}`);
